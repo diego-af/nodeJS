bbc07c17f6bef6a8b49eec0ce2b05aec750ca0d0
Diego Alves Ferreira <66676322+diego-af@users.noreply.github.com>
2021-11-24 08:08:10 -0300

    Atualizando com novas rotas

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || 3000
 
-const array=['Java','React Native','Musica']
+const porta = process.env.PORT || 3000
 
-app.get('/cursos/:index',(req,res)=>{
-    const {index} = req.params
+app.use(express.json())
 
-    res.json(array[index])
+/* Criando uos middlewares */
+
+function checkCurso(req,res,next){
+   const {name}= req.body
+
+   if(!req.body.name){
+       return res.status(404).json({error: 'Nada ecnobtrado'})
+   }
+   return next()
+
+}
+
+/* Array com os dados a ser passados */
+const cursos = ['Java', 'JavaScript', 'Mongodb']
+
+/* Retornando os dados */
+app.get('/cursos',(req,res)=>{
+    res.json(cursos)
 })
 
+/* Inserindo cursos com o Post */
+app.post('/cursos',checkCurso,(req,res)=>{
+    const {name}= req.body
 
+    cursos.push(name)
+    res.json(cursos)
+})
 
-app.listen(port)
+/* Atualizando o dados com o PUT */
+
+app.put('/cursos/:index',checkCurso,(req,res)=>{
+    const {index} = req.params
+    const {name} = req.body
+
+    cursos[index] = name
+    res.json(cursos)
+})
+app.listen(porta,()=>console.log("rodando o servidor"))
